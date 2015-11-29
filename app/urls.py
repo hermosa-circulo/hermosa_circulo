25fc1a8dec0c04ec9ea53905c4d93c14e54eb0f9
m-masataka <m.mizukoshi.wakuwaku@gmail.com>
2015-11-29 22:05:57 +0900

    revise

--- app/urls.py
+++ app/urls.py
@@ -3,10 +3,11 @@
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from app.views import MainView
+from app.views import *
 
 urlpatterns = patterns('',
     url(r'^$',MainView.as_view(), name='index'),
+    url(r'^boobs_blender/$',Boobs_BlenderView.as_view(), name='boobs_blender'),
     url(r'^update/$','app.views.update_3D_object', name='update'),
 #    url(r'^$','IGAView.as_view()', name = 'iga'),
 #    url(r'^$','Boobs_BlenderView.as_view()', name = 'boobs_blender'),
