596b25f3114f057e80315f677ceda2974534aa26
bandoshintaro <furainozako@gmail.com>
2016-01-23 02:12:20 +0900

    modified:   app/utils/Cylinder/Closoid.py  	modified:   app/utils/Cylinder/Vertex.py

--- app/utils/Cylinder/Closoid.py
+++ app/utils/Cylinder/Closoid.py
@@ -2,48 +2,41 @@ import sys
 import math
 
 def Kai(x):
-	if x ==0:
-		return 1
-	value = 0
-	if x == 1:
-		value = 1
-	else:
-		value = x * Kai(x-1)
-	return value
+    if x ==0: return 1
+    value = 1 if x == 1 else x * Kai(x-1)
+    return value
 
 def Closoid(tau):
-	A = 1
-	k = 1
-	x = 0
-	x_1 = 2
-	F_k_x = 0
-	F_k_y = 0
-	temp_f = 2
-	while(math.fabs(temp_f-F_k_x) > 0.00001):
-		temp_f = F_k_x
-		f_1 = math.pow(-1,k-1)
-		f_2 = 1.0/(Kai(2*(k-1))*(4*k-3))
-		f_3 = math.pow(tau,2*(k-1))
-		F_k_x += f_1*f_2*f_3
-		k = k+1
-		#print "%s %s %s" %(f_1,f_2,f_3)
-		#print F_k_x
-	temp_f = 2
-	k = 1
-	while(math.fabs(temp_f-F_k_y) > 0.00001):
-		temp_f = F_k_y
-		f_1 = math.pow(-1,k-1)
-		f_2 = 1.0/(Kai(2*k-1)*(4*k-1))
-		f_3 = math.pow(tau,2*(k-1))
-		F_k_y += f_1*f_2*f_3
-		k = k+1
-		#print "%s %s %s" %(f_1,f_2,f_3)
-		#print F_k_y
-	n =[1,1]
-	n[0] = F_k_x*A*math.sqrt(2*tau)
-	n[1] = F_k_y*A*tau*math.sqrt(2*tau)
-	return n
+    A = 1
+    k = 1
+    x = 0
+    x_1 = 2
+    F_k_x = 0
+    F_k_y = 0
+    temp_f = 2
+    while(math.fabs(temp_f-F_k_x) > 0.00001):
+        temp_f = F_k_x
+        f_1 = math.pow(-1,k-1)
+        f_2 = 1.0/(Kai(2*(k-1))*(4*k-3))
+        f_3 = math.pow(tau,2*(k-1))
+        F_k_x += f_1*f_2*f_3
+        k = k+1
+        #print "%s %s %s" %(f_1,f_2,f_3)
+        #print F_k_x
+    temp_f = 2
+    k = 1
+    while(math.fabs(temp_f-F_k_y) > 0.00001):
+        temp_f = F_k_y
+        f_1 = math.pow(-1,k-1)
+        f_2 = 1.0/(Kai(2*k-1)*(4*k-1))
+        f_3 = math.pow(tau,2*(k-1))
+        F_k_y += f_1*f_2*f_3
+        k = k+1
+        #print "%s %s %s" %(f_1,f_2,f_3)
+        #print F_k_y
+    n =[F_k_x*A*math.sqrt(2*tau),F_k_y*A*tau*math.sqrt(2*tau)]
+    return n
 def Formula(x):
-	point = Closoid((math.pi/60)*x)
-	n = [point[0]*100,point[1]*100]
-	return n
+    point = Closoid((math.pi/60)*x)
+    n = [point[0]*100,point[1]*100]
+    return n
--- app/utils/Cylinder/Vertex.py
+++ app/utils/Cylinder/Vertex.py
@@ -1,9 +1,10 @@
 import math
 import Closoid
+
 def Mapping(c,v):
-    for i in range(len(v)):
-        for j in range(len(v[0])):
-                v[i][j] = v[i][j] + c[j]
+    for i,row in enumerate(v):
+        for j,column in enumerate(row):
+            v[i][j] += c[j]
     return v
 def ring(r,point_num,center):
     z = 0
@@ -39,84 +40,78 @@ def pullV(V,point_num):
     stock = []
     num = 0
     for i in range(len(V)):
-	flags=1
-	if flags==1:
-		pull_len= 1*math.pow(0.1,i)
-		while len(stock)<point_num:
-			min = 100000
-			num = 0
-			for j in range(len(V[i])):
-				distance=50-V[i][j][1]
-				if distance < min and not j in stock:
-					min = distance
-					num = j
-			stock.append(num)
-		s=V[i][stock[0]][1]+pull_len
-		elen=1.5
-		dd=s-V[i][num][1]
-		nn= len(stock)/2
-		vs =[]
-		ss=99999999
-		sum =0
-		print "dd %s"%(dd)
-		print "stok"
-		for j in range(len(stock)):
-			print stock[j]
-		print "nn %s"%(nn)
-		for j in range(nn):
-			vs.append(sum)
-			ss = ss*0.9
-			sum =sum +ss
-		bb = dd/sum
-		print vs
-		for j in range(nn):
-			vs[j] = vs[j]*bb
-		print vs
-		count = len(vs)-1
-		for j in range(len(stock)):
-			V[i][stock[j]][1]=V[i][stock[j]][1]+vs[count]
-			print vs[count]
-			if j%2==1:
-				count=count-1
-		#	V[0][j][1]=V[0][j][1]+2
+        flags=1
+        if flags==1:
+            pull_len= 1*math.pow(0.1,i)
+            while len(stock)<point_num:
+                min = 100000
+                num = 0
+                for j in range(len(V[i])):
+                    distance=50-V[i][j][1]
+                    if distance < min and not j in stock:
+                        min = distance
+                        num = j
+                stock.append(num)
+            s=V[i][stock[0]][1]+pull_len
+            elen=1.5
+            dd=s-V[i][num][1]
+            nn= len(stock)/2
+            vs =[]
+            ss=99999999
+            sum =0
+            print "dd %s"%(dd)
+            print "stok"
+            for a in stock: print a[j]
+            print "nn %s"%(nn)
+            for j in range(nn):
+                vs.append(sum)
+                ss = ss*0.9
+                sum =sum +ss
+            bb = dd/sum
+            print vs
+            for j in range(nn): vs[j] = vs[j]*bb
+            print vs
+            count = len(vs)-1
+            for j in range(len(stock)):
+                V[i][stock[j]][1]=V[i][stock[j]][1]+vs[count]
+                print vs[count]
+                if j%2==1: count=count-1
+            #	V[0][j][1]=V[0][j][1]+2
     return V
 def makeR(x,r,p):
     x = x*p
-    if r > x:
-	y  = math.sqrt(math.pow(r,2) - math.pow(x,2))
-    else:
-	y = 0.5
+    y  = math.sqrt(math.pow(r,2) - math.pow(x,2)) if r > x: else 0.5
     return y
+
 def makeV(wheel_radius,begining_point,begin,point_num,breast_wide):
     flag = 1
     vertex=[]
     if flag ==1:
-	#print parameter.closoid_number
-	first_point = Closoid.Closoid((math.pi/200)*(0+begining_point))
-    	#first_point = [0,0]
-    	#print first_point
-    	#print parameter.parameter_num
-    	first_p2 = Closoid.Closoid((math.pi/200)*(begin+begining_point))
-    	#first_p2 = [0,0]
+        #print parameter.closoid_number
+        first_point = Closoid.Closoid((math.pi/200)*(0+begining_point))
+        #first_point = [0,0]
+        #print first_point
+        #print parameter.parameter_num
+        first_p2 = Closoid.Closoid((math.pi/200)*(begin+begining_point))
+        #first_p2 = [0,0]
 
-    	for i in range(150):
-        	r = makeR(i,wheel_radius,breast_wide)
-        	point = Closoid.Closoid((math.pi/200)*(i+begining_point))
-		#point = [0,0.01*i]
-		center = [0,-(point[0]-first_point[0]-first_p2[0])*100,(point[1]-first_point[1]-first_p2[1])*100]
-		#center = [0,0,i*5]
-		if begin < i and r > 0.5:
-			vertex.append(ring(r,point_num,center))
-	#vertex = pullV(vertex,point_num)
+        for i in range(150):
+            r = makeR(i,wheel_radius,breast_wide)
+            point = Closoid.Closoid((math.pi/200)*(i+begining_point))
+        #point = [0,0.01*i]
+        center = [0,-(point[0]-first_point[0]-first_p2[0])*100,(point[1]-first_point[1]-first_p2[1])*100]
+        #center = [0,0,i*5]
+        if begin < i and r > 0.5:
+            vertex.append(ring(r,point_num,center))
+        #vertex = pullV(vertex,point_num)
     else:
-	first_point = [0,0]
-	first_p2 = [0,0]
-	for i in range(150):
-		r = makeR(i,wheel_radius,breast_wide)
-		point = [0,0.01*i]
-		center = [30,10,i*1]
-		if begin < i and r>0.5:
-			vertex.append(ring(r,point_num,center))
-	vertex = pullV(vertex,point_num)
+        first_point = [0,0]
+        first_p2 = [0,0]
+        for i in range(150):
+            r = makeR(i,wheel_radius,breast_wide)
+            point = [0,0.01*i]
+            center = [30,10,i*1]
+            if begin < i and r>0.5:
+                vertex.append(ring(r,point_num,center))
+        vertex = pullV(vertex,point_num)
     return vertex
-
