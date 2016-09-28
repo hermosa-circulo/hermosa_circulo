4f6892e63aeec53f67f9ac602d414e0bee68bf48
root <root@ip-172-31-19-200.ap-northeast-1.compute.internal>
2016-09-28 09:46:54 +0000

    change code

--- app/utils/Cylinder/Vertex.py
+++ app/utils/Cylinder/Vertex.py
@@ -1,39 +1,21 @@
 import math
 import Closoid
 
-def Mapping(c,v):
-    for i,row in enumerate(v):
-        for j,column in enumerate(row):
-            v[i][j] += c[j]
-    return v
 def ring(r,point_num,center):
-    z = 0
-    a = 1
-    b = 1
-    y = -r
-    ver = []
-    rad = math.tan((math.pi/2))
-    x = math.sqrt(r*r/(1/math.pow(a,2)+rad*rad/(b*b)))
-
-    for i in range(point_num*2):
-        rad = math.tan((math.pi/point_num)*i)
-        x = math.sqrt(math.pow(r,2)/(1/math.pow(a,2)+math.pow(rad,2)/math.pow(b,2)))
-        y = math.sqrt((math.pow(r,2)-math.pow(x,2)/math.pow(a,2))/math.pow(b,2))
-        if (math.pi/point_num)*i < math.pi/2:
-            x = x
-            y = y
-        elif (math.pi/point_num)*i >= math.pi/2 and (math.pi/point_num)*i < math.pi:
-            x = -x
-            y = y
-        elif (math.pi/point_num)*i >= math.pi and (math.pi/point_num)*i < (math.pi/2)*3:
-            x = -x
-            y = -y
-        else:
-            x = x
-            y = -y
-        ver.append([x,y,z])
-    ver = Mapping(center,ver)
-    return ver
+    def ring(r,point_num,center):
+    array_v = []
+    theata = 0
+    fineness = 20
+    count = 0
+    while True:
+        x = r * math.cos(math.pi*fineness*count/360) + center[0]
+        y = r * math.sin(math.pi*fineness*count/360) + center[1]
+        z = center[2]
+        array_v.append([x,y,z])
+        if 720 == fineness*count:
+            break
+        count = count + 1
+    return array_v
 
 def makeR(x,r,p):
     x = x*p
