71fe7374b222fb019b8dc192070fc2e43313b239
root <root@ip-172-31-19-200.ap-northeast-1.compute.internal>
2016-10-01 16:05:41 +0000

    support django version 1.10

--- app/urls.py
+++ app/urls.py
@@ -4,13 +4,14 @@ from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from app.views import *
+from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$',MainView.as_view(), name='index'),
     url(r'^boobs_blender/$',Boobs_BlenderView.as_view(), name='boobs_blender'),
     url(r'^lattice/$',latticeView.as_view(),name='lattice'), 
-    url(r'^update/$','app.views.update_3D_object', name='update'),
-    url(r'^update_lattice_object/$','app.views.update_lattice_object', name='update_lattice_object'),
-    url(r'^makeBlender/$','app.views.executeBlender', name='makeBlender'),
-    url(r'^execIGA/$','app.views.executeIGA', name='execIGA'),
-)
+    url(r'^update/$',views.update_3D_object, name='update'),
+    url(r'^update_lattice_object/$',views.update_lattice_object, name='update_lattice_object'),
+    url(r'^makeBlender/$',views.executeBlender, name='makeBlender'),
+    url(r'^execIGA/$',views.executeIGA, name='execIGA'),
+]
